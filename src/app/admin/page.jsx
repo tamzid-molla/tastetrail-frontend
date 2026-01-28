d967de352f528fa6be033055d634c68c20098bd1
TAMZID <tamzid2233@gmail.com>
2026-01-29 00:07:39 +0600

    add admin and user dashboard

---
+++ src/app/admin/page.jsx
@@ -0,0 +1,10 @@
+
+const AdminPage = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-gray-800 mb-6">Admin Page</h1>
+        </div>
+    );
+};
+
+export default AdminPage;
